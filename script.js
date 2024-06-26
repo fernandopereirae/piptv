1596dd5b646ca38c7f5c94f5fe014cece4d555e2
pdl <69766117+fernandopereirae@users.noreply.github.com>
2024-06-26 19:34:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,63 +1,40 @@
 document.addEventListener("DOMContentLoaded", async function() {
     const loader = document.getElementById('loader');
     const categoryContainer = document.getElementById('category-container');
-    const logTextarea = document.getElementById('log'); // Elemento para exibir log de mensagens
 
     let baseURL, baseLogin, basePassword;
 
     // Função para fazer requisições fetch com timeout
-    function fetchWithTimeout(resource, options = {}) {
+    async function fetchWithTimeout(resource, options = {}) {
         const { timeout = 10000 } = options;
 
-        return new Promise((resolve, reject) => {
-            const controller = new AbortController();
-            const id = setTimeout(() => {
-                controller.abort();
-                reject(new Error('Timeout da requisição.'));
-            }, timeout);
+        const controller = new AbortController();
+        const id = setTimeout(() => {
+            controller.abort();
+        }, timeout);
 
-            fetch(resource, {
-                ...options,
-                signal: controller.signal
-            })
-            .then(response => {
-                clearTimeout(id);
-                if (!response.ok) {
-                    throw new Error('Erro na requisição: ' + response.statusText);
-                }
-                return response.text();
-            })
-            .then(resolve)
-            .catch(error => {
-                reject(error);
-            });
+        const response = await fetch(resource, {
+            ...options,
+            signal: controller.signal
         });
+
+        clearTimeout(id);
+
+        if (!response.ok) {
+            throw new Error('Erro na requisição: ' + response.statusText);
+        }
+
+        return await response.json();
     }
 
     // Função para carregar categorias
     async function loadCategories(url, login, password) {
-        try {
-            const response = await fetchWithTimeout(`${url}/player_api.php?username=${login}&password=${password}&action=get_live_categories`);
-            const categoriesData = JSON.parse(response);
-            return categoriesData;
-        } catch (error) {
-            const errorMessage = 'Erro ao buscar categorias: ' + error.message;
-            log(errorMessage);
-            throw error;
-        }
+        return await fetchWithTimeout(`${url}/player_api.php?username=${login}&password=${password}&action=get_live_categories`);
     }
 
     // Função para carregar canais de uma categoria específica
     async function loadChannels(url, login, password, categoryId) {
-        try {
-            const response = await fetchWithTimeout(`${url}/player_api.php?username=${login}&password=${password}&action=get_live_streams&category_id=${categoryId}`);
-            const channelsData = JSON.parse(response);
-            return channelsData;
-        } catch (error) {
-            const errorMessage = 'Erro ao buscar canais: ' + error.message;
-            log(errorMessage);
-            throw error;
-        }
+        return await fetchWithTimeout(`${url}/player_api.php?username=${login}&password=${password}&action=get_live_streams&category_id=${categoryId}`);
     }
 
     // Função para renderizar categorias e canais
@@ -93,8 +70,7 @@ document.addEventListener("DOMContentLoaded", async function() {
             // Esconder o loader após o carregamento
             loader.style.display = 'none';
         } catch (error) {
-            const errorMessage = 'Erro ao carregar categorias: ' + error.message;
-            log(errorMessage);
+            console.error('Erro ao carregar categorias:', error.message);
             categoryContainer.innerHTML = 'Erro ao carregar categorias. Tente novamente mais tarde.';
         }
     }
@@ -130,14 +106,6 @@ document.addEventListener("DOMContentLoaded", async function() {
         const streamURL = `${url}/live/${login}/${password}/${streamId}.m3u8`;
         sessionStorage.setItem('lastPlayedStream', streamURL);
         window.location.href = `playerv.html?streamUrl=${encodeURIComponent(streamURL)}`;
-    }
-
-    // Função para exibir mensagens de log
-    function log(message) {
-        const timestamp = new Date().toLocaleTimeString();
-        logTextarea.textContent += `[${timestamp}] ${message}\n`;
-        logTextarea.style.display = 'block'; // Garante que o log seja visível
-        console.log(message); // Também exibe no console para fins de depuração
     }
 
     // Iniciar a aplicação ao carregar o DOM
